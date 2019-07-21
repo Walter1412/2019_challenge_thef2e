a0178f8e4f6f1ae9ca3fdf6d8b857fa78f9ba103
Walter <walter@medialand.tw>
2019-07-21 23:46:45 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: ['vue', 'prettier'],
+  // add your custom rules here
+  rules: {
+    'nuxt/no-cjs-in-config': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'handle-callback-err': 'off',
+    'no-template-curly-in-string': 'off',
+    'camelcase': 'off',
+    'space-before-function-paren': 'off',
+    'curly': 'off',
+    'arrow-parens': 'off'
+  }
+}
